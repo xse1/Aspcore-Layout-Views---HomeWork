ef48fda3ea918d244605c6c699f340831e73700b
alali <alali@ABDULLAH>
2023-05-23 11:56:40 +0300

    Add project files.

---
+++ AB/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using AB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AB.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        
+    }
+}
